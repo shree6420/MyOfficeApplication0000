4844a666fa99250128c16db0254c22d018188c59
Rahul9545 <99891574+Rahul9545@users.noreply.github.com>
2023-08-16 12:35:45 +0530

    committing changes : image zoom in , zoom out

--- app/src/main/java/com/example/myofficeapplication2/activity/DashboardActivity.java
+++ app/src/main/java/com/example/myofficeapplication2/activity/DashboardActivity.java
@@ -3,8 +3,10 @@ package com.example.myofficeapplication2.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
@@ -18,6 +20,20 @@ public class DashboardActivity extends AppCompatActivity {
     private Context context;
     private AutoCompleteTextView edtSelectState, edtSelectDist;
     private ImageView imageMap;
+    float[] lastEvent = null;
+    float d = 0f;
+    float newRot = 0f;
+    private boolean isZoomAndRotate;
+    private boolean isOutSide;
+    private static final int NONE = 0;
+    private static final int DRAG = 1;
+    private static final int ZOOM = 2;
+    private int mode = NONE;
+    private PointF start = new PointF();
+    private PointF mid = new PointF();
+    float oldDist = 1f;
+    private float xCoOrdinate, yCoOrdinate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +50,16 @@ public class DashboardActivity extends AppCompatActivity {
     }
 
     private void setOnClick() {
+
+        imageMap.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                ImageView view = (ImageView) v;
+                view.bringToFront();
+                viewTransformation(view, event);
+                return true;
+            }
+        });
         edtSelectState.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -87,5 +113,86 @@ public class DashboardActivity extends AppCompatActivity {
                 popup.show();
             }
         });
+    }
+
+    private void viewTransformation(View view, MotionEvent event) {
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                xCoOrdinate = view.getX() - event.getRawX();
+                yCoOrdinate = view.getY() - event.getRawY();
+
+                start.set(event.getX(), event.getY());
+                isOutSide = false;
+                mode = DRAG;
+                lastEvent = null;
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                oldDist = spacing(event);
+                if (oldDist > 10f) {
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                }
+
+                lastEvent = new float[4];
+                lastEvent[0] = event.getX(0);
+                lastEvent[1] = event.getX(1);
+                lastEvent[2] = event.getY(0);
+                lastEvent[3] = event.getY(1);
+                d = rotation(event);
+                break;
+            case MotionEvent.ACTION_UP:
+                isZoomAndRotate = false;
+                if (mode == DRAG) {
+                    float x = event.getX();
+                    float y = event.getY();
+                }
+            case MotionEvent.ACTION_OUTSIDE:
+                isOutSide = true;
+                mode = NONE;
+                lastEvent = null;
+            case MotionEvent.ACTION_POINTER_UP:
+                mode = NONE;
+                lastEvent = null;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (!isOutSide) {
+                    if (mode == DRAG) {
+                        isZoomAndRotate = false;
+                        view.animate().x(event.getRawX() + xCoOrdinate).y(event.getRawY() + yCoOrdinate).setDuration(0).start();
+                    }
+                    if (mode == ZOOM && event.getPointerCount() == 2) {
+                        float newDist1 = spacing(event);
+                        if (newDist1 > 10f) {
+                            float scale = newDist1 / oldDist * view.getScaleX();
+                            view.setScaleX(scale);
+                            view.setScaleY(scale);
+                        }
+                        if (lastEvent != null) {
+                            newRot = rotation(event);
+                            view.setRotation((float) (view.getRotation() + (newRot - d)));
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private float rotation(MotionEvent event) {
+        double delta_x = (event.getX(0) - event.getX(1));
+        double delta_y = (event.getY(0) - event.getY(1));
+        double radians = Math.atan2(delta_y, delta_x);
+        return (float) Math.toDegrees(radians);
+    }
+
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (int) Math.sqrt(x * x + y * y);
+    }
+
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
     }
 }
